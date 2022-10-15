205f24324c3b68150381eafa92f1c4237fea1311
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-10-15 14:48:39 -0700

    stuff, lots of it

--- Assets/Scripts/BRAgent.cs
+++ Assets/Scripts/BRAgent.cs
@@ -8,8 +8,23 @@ using Unity.MLAgents.Sensors;
 [RequireComponent(typeof(RayPerceptionSensorComponent3D), typeof(DecisionRequester))]
 public class BRAgent : Agent
 {
-    [SerializeField] EnvManager env;
-    [SerializeField] SpriteRenderer bg;
+    [Header("Management")]
+
+    [SerializeField] BRManager env;
+
+    [Header("Gameplay")]
+
+    Rigidbody rb;
+    [SerializeField] float speed = 5f;
+    [SerializeField] float maxTurnSpeed = 5f;
+
+    [SerializeField] LayerMask targetableLayers;
+    [SerializeField] float gunCooldownInSeconds = 3f;
+    float cooldown = 0;
+
+    AudioSource audio;
+    [SerializeField] AudioClip shootSound;
+    [SerializeField] AudioClip killSound;
 
     private void Awake()
     {
@@ -18,16 +33,17 @@ public class BRAgent : Agent
     // Can be used as the Start() function
     public override void Initialize()
     {
-
+        rb = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
     }
 
     // Runs every time the training environment is reset (when a new episode begins) 
     public override void OnEpisodeBegin()
     {
-
+        env.ResetStage();
     }
 
-    /// The following 3 functions can be seen as running in the Update() loop: 
+    /// The following 3 functions can be seen as running like the Update() loop, except they run as frequently as you collect observations: 
     /// first, CollectObservations() gets the input vector for the DQN, 
     /// then Heuristic() can override the actual output action vector (basically for testing only)
     /// finally, OnActionReceived() takes the output action vector and moves the Agent
@@ -36,35 +52,42 @@ public class BRAgent : Agent
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.position.x);
-        sensor.AddObservation(transform.position.y);
+        sensor.AddObservation(transform.position.z);
+        sensor.AddObservation(Physics.Raycast(transform.position, transform.forward, 200f, targetableLayers));
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        var discOut = actionsOut.DiscreteActions;
+        var contOut = actionsOut.ContinuousActions;
 
-        discOut[0] = (int)Input.GetAxisRaw("Horizontal");
-        discOut[1] = (int)Input.GetAxisRaw("Vertical");
+        contOut[0] = Input.GetAxisRaw("Horizontal");
+        contOut[1] = Input.GetAxisRaw("Vertical");
+        contOut[2] = 0; // Placeholder
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        transform.position += new Vector3(actions.DiscreteActions[0] - 1, actions.DiscreteActions[1] - 1, 0) * Time.deltaTime;
+        // transform.position += new Vector3(actions.DiscreteActions[0] - 1, actions.DiscreteActions[1] - 1, 0) * Time.deltaTime;
+        rb.velocity = new Vector3(Mathf.Clamp(actions.ContinuousActions[0], -1f, 1f) * speed, rb.velocity.y, Mathf.Clamp(actions.ContinuousActions[1], -1f, 1f) * speed);
+        rb.angularVelocity = new Vector3(rb.angularVelocity.x, Mathf.Clamp(actions.ContinuousActions[2], -1f, 1f) * maxTurnSpeed, rb.angularVelocity.z);
+        if (actions.DiscreteActions[0] == 0)
+        {
+            Shoot();
+        }
     }
 
-    public void OnCollisionEnter2D(Collision2D other)
+    public void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("boundary"))
         {
-            AddReward(-50f);
-            EndEpisode();
+            //AddReward(-50f);
+            //EndEpisode();
         }
     }
 
     public void Win()
     {
         AddReward(50f);
-        bg.color = Color.green;
         EndEpisode();
     }
 
@@ -72,4 +95,35 @@ public class BRAgent : Agent
     {
         AddReward(10f);
     }
+
+    private void Update()
+    {
+        if (cooldown > 0)
+            cooldown -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        RaycastHit info;
+        if (cooldown > 0)
+        {
+            return;
+        } else
+        {
+            cooldown = gunCooldownInSeconds;
+        }
+
+        if (Physics.Raycast(transform.position, transform.forward, out info, 200f, targetableLayers))
+        {
+            Debug.Log(info.collider);
+            env.RegisterHit(this, info.collider.gameObject.GetComponent<BRAgent>());
+            audio.PlayOneShot(killSound);
+
+        } else
+        {
+            // audio.PlayOneShot(shootSound);
+            AddReward(-5f);
+        }
+    }
+
 }
---
+++ Assets/Scripts/BRManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BRManager : MonoBehaviour
+{
+    private int numAlive;
+
+    [SerializeField] BRAgent enemy0;
+    [SerializeField] BRAgent enemy1;
+
+    Vector3 enemy0Start;
+    Vector3 enemy1Start;
+
+
+    public int enemyCount
+    {
+        get
+        {
+            // Return number of alive enemies, so -1 because one is always yourself.
+            return numAlive - 1;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy0Start = enemy0.transform.position;
+        enemy1Start = enemy1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Register what agent hit which other agent 
+    public void RegisterHit(BRAgent src, BRAgent targ)
+    {
+        // Right now for 2 player duel: Reward winning agent and end episode
+        src.Win();
+        targ.EndEpisode();
+    }
+
+    public void ResetStage()
+    {
+        enemy0.transform.position = enemy0Start;
+        enemy1.transform.position = enemy1Start;
+    }
+}
