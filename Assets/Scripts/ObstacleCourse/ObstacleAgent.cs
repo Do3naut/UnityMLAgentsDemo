b41cef0340f2832e2f26600cc720496bf74acbdc
henrywangzh <henrywang3@g.ucla.edu>
2022-12-26 23:33:08 -0500

    ah ha

--- Assets/Scripts/ObstacleCourse/ObstacleAgent.cs
+++ Assets/Scripts/ObstacleCourse/ObstacleAgent.cs
@@ -12,16 +12,32 @@ public class ObstacleAgent : Agent
     [SerializeField] float speed = 5f;
     [SerializeField] float maxTurnSpeed = 5f;
 
+    [SerializeField] private Transform targetTransform;
+
+    [SerializeField] private Material winMaterial, loseMaterial;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
+    private RayPerceptionSensorComponent3D RaySensor;
+
+    private Vector3 startingPos;
+
+    private float startingDistance;
+    private Vector3 lastPos;
+
     // Can be used as the Start() function
     public override void Initialize()
     {
         rb = GetComponent<Rigidbody>();
+        startingPos = new Vector3(9, 0.5f, -9);
+        RaySensor = GetComponent<RayPerceptionSensorComponent3D>();
+        startingDistance = Vector3.Distance(transform.localPosition, targetTransform.localPosition);
+        lastPos = transform.localPosition;
     }
 
     // Runs every time the training environment is reset (when a new episode begins) 
     public override void OnEpisodeBegin()
     {
-
+        transform.localPosition = startingPos;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
     }
 
     /// The following 3 functions can be seen as running like the Update() loop, except they run as frequently as you collect observations: 
@@ -32,8 +48,9 @@ public class ObstacleAgent : Agent
     // Collect input observations (corresponding with the BrainParameters property above)
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(transform.position.x);
-        sensor.AddObservation(transform.position.z);
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition - transform.localPosition);
+        sensor.AddObservation(transform.rotation.eulerAngles);
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
@@ -47,21 +64,50 @@ public class ObstacleAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
+        float prevDistance = Vector3.Distance(lastPos, targetTransform.localPosition);
+        float currDistance = Vector3.Distance(transform.localPosition, targetTransform.localPosition);
+        AddReward((prevDistance-currDistance)/10f);
+        Debug.Log(GetCumulativeReward());
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+        float rotation = actions.ContinuousActions[2];
         // Our agent will be able to freely control its non-vertical movement.
-        rb.velocity = new Vector3(Mathf.Clamp(actions.ContinuousActions[0], -1f, 1f) * speed, rb.velocity.y, Mathf.Clamp(actions.ContinuousActions[1], -1f, 1f) * speed);
-        rb.angularVelocity = new Vector3(rb.angularVelocity.x, Mathf.Clamp(actions.ContinuousActions[2], -1f, 1f) * maxTurnSpeed, rb.angularVelocity.z);
+        
+        rb.velocity = transform.right * Mathf.Clamp(moveX, -1f, 1f) * speed + transform.forward * Mathf.Clamp(moveZ, -1f, 1f) * speed;
+
+        Vector3 angleDelta = new Vector3(0, Mathf.Clamp(rotation, -1f, 1f) * maxTurnSpeed, 0);
+        transform.Rotate(angleDelta);
+        //rb.angularVelocity = new Vector3(rb.angularVelocity.x, Mathf.Clamp(rotation, -1f, 1f) * maxTurnSpeed, rb.angularVelocity.z);
+
+        lastPos = transform.localPosition;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("boundary"))
         {
-            AddReward(-2f);
-            //EndEpisode();
+            AddReward(-0.2f);
         }
         if (collision.gameObject.CompareTag("Finish"))
         {
-            AddReward(50f);
+            AddReward(100f);
+            EndEpisode();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("boundary"))
+        {
+            AddReward(-50f);
+            floorMeshRenderer.material = loseMaterial;
+            EndEpisode();
+
+        }
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            AddReward(100f);
+            floorMeshRenderer.material = winMaterial;
             EndEpisode();
         }
     }
